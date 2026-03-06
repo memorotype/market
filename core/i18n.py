1398721e3f6263993386fcb823c74d421e92abcc
memorotype <karakocyekta@gmail.com>
2026-03-06 12:48:49 +0100

    Create i18n.py

---
+++ core/i18n.py
@@ -0,0 +1,22 @@
+UI_TEXT = {
+    "tr": {
+        "add_item": "Ürün ekle",
+        "qty": "Miktar",
+        "unit": "Ölçü",
+        "note": "Not",
+        "todo": "Alınacaklar",
+        "history": "Son Alınanlar",
+        "your_lang": "Senin dilin",
+        "household": "Hane kodu",
+    },
+    "ru": {
+        "add_item": "Добавить товар",
+        "qty": "Количество",
+        "unit": "Ед.",
+        "note": "Заметка",
+        "todo": "Купить",
+        "history": "Недавно купленные",
+        "your_lang": "Ваш язык",
+        "household": "Код семьи",
+    },
+}
